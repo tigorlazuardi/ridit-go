e20553bea2313bacce82d2a9c4b6b54a55507f6f
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-07-11 21:46:22 +0700

    add download configuration

--- app/cli/config/download.go
+++ app/cli/config/download.go
@@ -1,6 +1,16 @@
 package config
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/tigorlazuardi/ridit/app/config"
+	"github.com/tigorlazuardi/ridit/app/config/models"
+	"github.com/tigorlazuardi/ridit/pkg"
+)
 
 var downloadConfig = &cobra.Command{
 	Use:   "download",
@@ -15,7 +25,18 @@ var pathConfig = &cobra.Command{
 	Short:   "sets download path",
 	Example: "ridit config download path '~/Pictures'",
 	Run: func(cmd *cobra.Command, args []string) {
-		_ = cmd.Help()
+		if len(args) < 1 {
+			_ = cmd.Help()
+			os.Exit(1)
+		}
+		entry := pkg.EntryFromContext(cmd.Context())
+		err := config.Modify(viper.GetString("profile"), func(c *models.Config) {
+			c.Download.Path = args[0]
+		})
+		if err != nil {
+			entry.Fatal(err)
+		}
+		fmt.Println("download path set to ", args[0])
 	},
 }
 
@@ -24,7 +45,25 @@ var connectTimeout = &cobra.Command{
 	Short:   "Duration to wait establishing connection to source download",
 	Example: "ridit config download connect_timeout 5s",
 	Run: func(cmd *cobra.Command, args []string) {
-		_ = cmd.Help()
+		if len(args) < 1 {
+			_ = cmd.Help()
+			os.Exit(1)
+		}
+
+		entry := pkg.EntryFromContext(cmd.Context())
+
+		t, err := time.ParseDuration(args[0])
+		if err != nil {
+			entry.Fatal(err)
+		}
+		err = config.Modify(viper.GetString("profile"), func(c *models.Config) {
+			c.Download.ConnectTimeout.Duration = t
+		})
+		if err != nil {
+			entry.Fatal(err)
+		}
+
+		fmt.Println("download connection timeout set to ", args[0])
 	},
 }
 
@@ -33,7 +72,25 @@ var downloadTimeout = &cobra.Command{
 	Short:   "Duration to wait for download to finish. Increase if you have slow connection",
 	Example: "ridit config download timeout 30s",
 	Run: func(cmd *cobra.Command, args []string) {
-		_ = cmd.Help()
+		if len(args) < 1 {
+			_ = cmd.Help()
+			os.Exit(1)
+		}
+
+		entry := pkg.EntryFromContext(cmd.Context())
+
+		t, err := time.ParseDuration(args[0])
+		if err != nil {
+			entry.Fatal(err)
+		}
+		err = config.Modify(viper.GetString("profile"), func(c *models.Config) {
+			c.Download.Timeout.Duration = t
+		})
+		if err != nil {
+			entry.Fatal(err)
+		}
+
+		fmt.Println("download timeout set to ", args[0])
 	},
 }
 
